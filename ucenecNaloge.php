478ce79489e9477afb01e2b239ea1a9704c76c45
UrbanL5 <urban.levicar.16872@dijak.sc-celje.si>
2022-11-01 22:23:24 +0100

    Začel delati sistem za nalaganje datotek.

--- ucenecNaloge.php
+++ ucenecNaloge.php
@@ -9,7 +9,7 @@ if (isset($_GET['predmet'])) {
 if (isset($_GET['naloga'])) {
     $naloga = $_GET['naloga'];
     echo $naloga;
-}else{
+} else {
     header("location:ucenecPredmeti.php?predmet='$predmet'");
 }
 if (!isset($_SESSION["username"])) {
@@ -24,6 +24,33 @@ if ($_SESSION['stopnja'] == 2) {
 }
 $connect = mysqli_connect("localhost", "basicuser", "edD-AgA_FeFfqjOC", "moodle");
 
+if (isset($_POST['submit'])) {
+    $file = $_FILES['file'];
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+    if (in_array($fileActualExt, $allowed)) {
+        if ($fileError === 0) {
+            if ($fileSize < 50000) {
+                $fileNameNew = uniqid('', true) . "." . $fileActualExt;
+                $fileDestination = 'nalogeUcencev/' . $fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+            } else {
+                echo "Your file is too big!";
+            }
+        } else {
+            echo "There was an error uploading your file!";
+        }
+    } else {
+        echo "You cannot upload files of this type!";
+    }
+}
 
 ?>
 <!doctype html>
@@ -37,11 +64,20 @@ $connect = mysqli_connect("localhost", "basicuser", "edD-AgA_FeFfqjOC", "moodle"
 </head>
 <body>
 <?php
-    $result = mysqli_query($connect, "SELECT * FROM naloga WHERE prikazan_naslov = '$naloga'");
-    $result = mysqli_fetch_assoc($result);
-    echo $result['naslov'];
-    echo $result['navodilo'];
-    echo $result['rok_oddaje'];
+$result = mysqli_query($connect, "SELECT * FROM naloga WHERE prikazan_naslov = '$naloga'");
+$result = mysqli_fetch_assoc($result);
+echo $result['naslov'];
+echo $result['navodilo'];
+echo $result['rok_oddaje'];
+
 ?>
+<form action="upload.php" method="POST" enctype="multipart/form-data" ›
+<label>
+    <input type="file" name="file" style="display:block">
+</label>
+<label>
+    <input type="submit" name="submit">
+</label></form>
+
 </body>
 </html>

95299737f7588cb5be9dae2497ef0a9b84f1fffc
CigoJabi <tilen.konig.16865@dijak.sc.celje.si>
2022-11-04 16:08:55 +0100

    css files

---
+++ footer.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<HTML>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/7dbc295856.js" crossorigin="anonymous"></script>
+    <title>e-redovalnica</title>
+
+    <link rel="stylesheet" href="footer.css">
+
+</head>
+<div class="niggy">
+    <footer>
+        <div class="footer-content">
+            <h3>e-redovalnica</h3>
+            <ul class="povezave">
+                <li>
+                    <a href="#"> <i class="fa fa-facebook"></i> </a>
+                </li>
+                <li>
+                    <a href="#"> <i class="fa fa-twitter"></i> </a>
+                </li>
+                <li>
+                    <a href="#"> <i class="fa fa-instagram"></i> </a>
+                </li>
+                <li>
+                    <a href="#"> <i class="fa fa-youtube"></i> </a>
+                </li>
+                <li>
+                    <a href="#"> <i class="fa fa-linkedin-square"></i> </a>
+                </li>
+            </ul>
+        </div>
+        <div class="footer-spodaj">
+            <p>e-redovalnica d.o.o 2022 © <span>Tilen König</span></p>
+        </div>
+
+    </footer>
+</div>
